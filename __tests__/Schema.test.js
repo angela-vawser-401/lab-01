769de8f2595d140f5cb5917c8ae388c8401806c7
Angela Vawser <amvawser@gmail.com>
2019-09-18 18:35:58 -0700

    schema and tests are passing

--- __tests__/Schema.test.js
+++ __tests__/Schema.test.js
@@ -1,23 +1,52 @@
 const Schema = require('../lib/Schema');
 
 describe('Schema', () => {
+  const personSchema = {
 
+    firstName: {
+      type: 'string',
+      required: true,
+    },
+
+    lastName: {
+      type: 'string',
+      required: true,
+    },
+      
+    married: {
+      type: 'boolean',
+      required: true,
+    },
+
+    kids: {
+      type: 'number',
+      required: true,
+    },
+
+  };
+
+  const newSchema = new Schema(personSchema);
+
+  const newDataSample = {
+    
+    'firstName': 'Angela',
+    'lastName': 'Vawser',
+    'married': true,
+    'kids': 0,
+  };
+
+  const needsCoersionSample = {
+    
+    'firstName': 'Angela',
+    'lastName': 'Vawser',
+    'married': 'true',
+    'kids': '0',
+  };
+  
   // add a test schema
 
-  it('strings', () => {
-    expect(validator.getCaster('string')).toBe(validator.toString);
+  it('validates model', () => {
+    expect(newSchema.validate(newDataSample)).toEqual(newDataSample);
+    expect(newSchema.validate(needsCoersionSample)).toEqual(newDataSample);
   });
-  
-  it('numbers', () => {
-    expect(validator.getCaster('number')).toBe(validator.toNumber);
-  });
-
-  it('booleans', () => {
-    expect(validator.getCaster('boolean')).toBe(validator.toBoolean);
-  });
-
-  it('date', () => {
-    expect(validator.getCaster('date')).toBe(validator.toDate);
-  });
-
 });
